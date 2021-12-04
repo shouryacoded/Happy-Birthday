7b6b0cf9baaa4f02d5081cbbccbfebf0f4b0f1b9
shouryacoded <shourya.online23@gmail.com>
2021-12-04 15:18:07 +0530

    Button is working and responding to inputs

--- app/src/main/java/com/shourya/happybirthday/MainActivity.kt
+++ app/src/main/java/com/shourya/happybirthday/MainActivity.kt
@@ -2,10 +2,22 @@ package com.shourya.happybirthday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val response = findViewById<Button>(R.id.Button)
+        var number = 0
+        response.setOnClickListener{
+            number++
+            Toast.makeText(this,"Push Harder $number",Toast.LENGTH_SHORT).show()
+            if (number == 5) {
+                Toast.makeText(this, "Better luck next time", Toast.LENGTH_SHORT).show()
+                number = 0
+            }
+        }
     }
 }
